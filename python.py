f7df205837b028fe88e4fb35834f5b6dc5407b87
chuanjin <thomas.chenchuanjin@gmail.com>
2016-11-09 10:36:08 +0800

    ok

---
+++ python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print u"变一个"
+print u'你是谁'
+print u"在吗！"
+print u'你是谁.'
+print u"段嘉敏"
